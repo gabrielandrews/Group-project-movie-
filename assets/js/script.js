f78e6106132a0a535512e072fe5df0b3ac547e61
ekball <90882418+ekball@users.noreply.github.com>
2021-11-22 20:50:27 -0500

    modified code to reflect changes to api's... new api added in new function of nested fetch requests and the code will now print 1 random movie title/poster along with 4 release years (one of them being the correct choice)

--- assets/js/script.js
+++ assets/js/script.js
@@ -134,9 +134,139 @@ function getPosterApi() {
 
 };
 
+function getReleaseYearApi() {
+
+    // use the IMDB BoxOfficeAllTime Api to get a title and poster 
+    var requestUrl = "https://imdb-api.com/en/API/BoxOfficeAllTime/k_un7r1xw2"
+
+    fetch(requestUrl)
+      .then(function (response) {
+        return response.json();
+      })
+
+      .then(function (data) {
+        // console.log(data);
+
+        // save a random number to a variable that will correspond to the index of the array api
+        var thisMovie = randomize (200);
+        var randomChoice1 = randomize (200);
+        var randomChoice2 = randomize (200);
+        var randomChoice3 = randomize (200);
+
+        // save the title and id of the movie (randomly chosen out of the 200 available in the api) to variables       
+        var movieTitle = data.items[thisMovie].title;
+        var movieId = data.items[thisMovie].id;
+
+        var randomMovieId1 = data.items[randomChoice1].id;
+        var randomMovieId2 = data.items[randomChoice2].id;
+        var randomMovieId3 = data.items[randomChoice3].id;
+        
+        // set title of fourth button
+        $('#title').text(movieTitle);
+
+        // create a new api url for the movie poster api (IMDB) 
+        var moviePosterUrl = "https://imdb-api.com/en/API/Posters/k_un7r1xw2/" + movieId;
+
+        // create a new api url for the movie release year api (OMDB)
+        var truemovieYearUrl = "http://www.omdbapi.com/?i=" + movieId + "&apikey=83e56856";
+
+        var falseChoiceUrl1 = "http://www.omdbapi.com/?i=" + randomMovieId1 + "&apikey=83e56856";
+        var falseChoiceUrl2 = "http://www.omdbapi.com/?i=" + randomMovieId2 + "&apikey=83e56856";
+        var falseChoiceUrl3 = "http://www.omdbapi.com/?i=" + randomMovieId3 + "&apikey=83e56856";
+
+        // fetch new data about movie and get year of release from omdb api
+        fetch(truemovieYearUrl)
+            .then(function (response) {
+            return response.json();
+            })
+
+            .then(function (data) {
+            // Use the console to examine the response
+            console.log(data);
+
+            // save the first poster in the movie's array to a variable
+            // var movieReleaseYear1 = data.posters[0].link;
+
+            })
+
+        // fetch new data about movie and get year of release from omdb api
+        fetch(falseChoiceUrl1)
+            .then(function (response) {
+            return response.json();
+            })
+
+            .then(function (data) {
+            // Use the console to examine the response
+            // console.log(data);
+
+            // save the first poster in the movie's array to a variable
+            // var movieReleaseYear1 = data.posters[0].link;
+
+            })
+
+        // fetch new data about movie and get year of release from omdb api
+        fetch(falseChoiceUrl2)
+            .then(function (response) {
+            return response.json();
+            })
+
+            .then(function (data) {
+            // Use the console to examine the response
+            // console.log(data);
+
+            // save the first poster in the movie's array to a variable
+            // var movieReleaseYear1 = data.posters[0].link;
+
+            })
+
+        // fetch new data about movie and get year of release from omdb api
+        fetch(falseChoiceUrl3)
+            .then(function (response) {
+            return response.json();
+            })
+
+            .then(function (data) {
+            // Use the console to examine the response
+            // console.log(data);
+
+            // save the first poster in the movie's array to a variable
+            // var movieReleaseYear1 = data.posters[0].link;
+
+            })
+
+
+
+
+
+            
+        // fetch new data about movie and get poster image
+        fetch(moviePosterUrl)
+            .then(function (response) {
+            return response.json();
+            })
+
+        .then(function (data) {
+            // Use the console to examine the response
+            // console.log(data);
+
+            // save the first poster in the movie's array to a variable
+            var moviePoster = data.posters[0].link;
+
+            // set the background of the fourth button to the movie poster for the fourth film
+            $('#title').css("background-image", "url(" + moviePoster + ")");
+
+        })
+
+    })
+
+
+};
+
+
 $("#random-button").click(function () {
 
-    getPosterApi();
+    // getPosterApi();
 
+    getReleaseYearApi();
 });
 
